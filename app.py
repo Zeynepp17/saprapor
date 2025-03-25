497902826e12b6342eb2aa86f14dd701e2f23d7e
Zeynep Kiral <zeynpkrall@gmail.com>
2025-03-25 11:31:30 +0300

    Komut düzeltildi

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
-import pandas as pd
+import pandas as pd 
 
+st.set_page_config(page_title= "RAPOR", layout= "wide")
 uploaded_file = st.file_uploader("Excel Dosyanizi Yükleyin", type=["xlsx"])
 
 if uploaded_file is not None:
