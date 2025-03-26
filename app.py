114c18649e77fc3759ca16eb431cb4af0a89afb1
Zeynep Kiral <zeynpkrall@gmail.com>
2025-03-26 11:04:44 +0300

    Kodda iyileştirme yapıldı

--- app.py
+++ app.py
@@ -11,45 +11,47 @@ else:
     df = pd.read_excel("ZFMR0003 Raporu Örnek.xlsx")
     st.info("Dummy Dosyasi Gösteriliyor.")
 
-st.title(" SAP Raporu Dinamik Karşılaştırma")
-
-
+st.title("SAP Raporu Dinamik Karşılaştırma")
 
 aylar = ["Ocak", "Şubat", "Mart", "Nisan", "Mayıs", "Haziran",
          "Temmuz", "Ağustos", "Eylül", "Ekim", "Kasım", "Aralık"]
 
-ay1 = st.selectbox("1. Ayı Seçin", aylar, index=0)
-ay2 = st.selectbox("2. Ayı Seçin", aylar, index=1)
+secilen_aylar= st.multiselect("Ay(lar) seçin", aylar, default=["Ocak","Şubat"]) #aylar seciliyor
 
+kisi_listesi_dosyadan = df["Defter"].dropna().unique().tolist()
+secili_kisiler = st.multiselect("Kişi Seç", kisi_listesi_dosyadan)
 
-
-veri_turleri = ["Bütçe", "Fiili", "Bütçe Bakiye", "Karşılık", "ÇKG", ]
+veri_turleri = ["Bütçe", "Fiili", "Bütçe Bakiye", "Karşılık", "ÇKG"]
 secilen_veriler = st.multiselect("Hangi veri türlerini görmek istiyorsunuz?", veri_turleri, default=["Bütçe", "Fiili"])
 
 
-
-sabit_sutunlar = list(df.columns[:11])
-secilen_sabitler = st.multiselect("Eklemek istediğiniz sabit sütunları seçin", sabit_sutunlar, default=["Masraf Yeri Adı"])
-
-def get_sutunlar_for_ay(ay, turler):
-    return [f"{ay} {tur}" for tur in turler]
-
-sutunlar_ay1 = get_sutunlar_for_ay(ay1, secilen_veriler)
-sutunlar_ay2 = get_sutunlar_for_ay(ay2, secilen_veriler)
-
-
-sutunlar_ay1 = [s for s in sutunlar_ay1 if s in df.columns]
-sutunlar_ay2 = [s for s in sutunlar_ay2 if s in df.columns]
+sabit_sutunlar = list(df.columns[:11]) + ["Defter"]
+secilen_sabitler = st.multiselect("Eklemek istediğiniz sabit sütunları seçin", sabit_sutunlar, default=["Masraf Yeri Adı", "Defter"])
 secilen_sabitler = [s for s in secilen_sabitler if s in df.columns]
 
+tum_sutunlar_aylar = []
+for ay in secilen_aylar:
+    for tur in secilen_veriler:
+        sutun = f"{ay} {tur}"
+        if sutun in df.columns:
+            tum_sutunlar_aylar.append(sutun)
 
-tum_sutunlar = secilen_sabitler + sutunlar_ay1 + sutunlar_ay2
-# 
+
 kumule_sutunlar = [col for col in df.columns if "Kümüle" in col]
 secilen_kumule = st.multiselect("Kümüle verilerinden hangilerini görmek istersiniz?", kumule_sutunlar)
 
-# 
-tum_sutunlar = secilen_sabitler + sutunlar_ay1 + sutunlar_ay2 + secilen_kumule
 
-st.subheader(f" {ay1} ve {ay2} Final Tablo")
-st.dataframe(df[tum_sutunlar])
+tum_sutunlar = secilen_sabitler + tum_sutunlar_aylar + secilen_kumule
+
+
+tum_df_listesi = []
+for kisi in secili_kisiler:
+    df_filtered = df[df["Defter"] ==kisi].copy()
+    tum_df_listesi.append(df_filtered)
+
+if tum_df_listesi:
+    df_secilikisiler = pd.concat(tum_df_listesi, ignore_index=True)
+    st.subheader("Final Tablo")
+    st.dataframe(df_secilikisiler[tum_sutunlar])
+else:
+    st.info("Lütfen en az 1 kişi seçin.")
